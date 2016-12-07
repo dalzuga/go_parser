2c51a07341d68e4784d8e7b685a392b0d86b3aca
Daniel Alzugaray <dalzuga@gmail.com>
2016-12-07 13:39:22 -0800

    Using Scanner to read.

--- go_parser.go
+++ go_parser.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io/ioutil"
 	"log"
+	"os"
 )
 
 func main() {
 	fmt.Printf("hello, world\n")
-	f, err := ioutil.ReadFile("books.xml")
+	f, err := os.Open("books.xml")
 
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%s", f)
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text()) // Println will add back the final '\n'
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
 }
