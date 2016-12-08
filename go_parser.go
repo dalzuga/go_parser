1e5d61c8b41dd1f3f0fcd1c3b04641ef14200fa4
Daniel Alzugaray <dalzuga@gmail.com>
2016-12-07 22:02:33 -0800

    Added comments explaining the regular expression.

--- go_parser.go
+++ go_parser.go
@@ -17,6 +17,20 @@ func main() {
 
 	scanner := bufio.NewScanner(f)
 
+	/*
+	 * This regular expression captures XML tags according to the XML spec.
+	 * XML tags can start with '<' or '</'. Thus: '</?'
+	 * The first character cannot be a punctuation character or a number,
+	 * and it must not be empty. Thus: '[A-Za-z]{1}'.
+	 * The '*?>' section is used for lazy matching until the closing '>'
+	 * character.
+	 * The parentheses '(', ')' are used for capturing and later extracting via
+	 * the FindStringSubmatch function.
+	 * The '[^\t\n\f\r ]' group matches anything that is not whitespace;
+	 * together, with the placement of the closing parenthesis, this allows
+	 * for not capturing attributes.
+	 */
+
 	captureTagRegExp := "</?([A-Za-z]{1}[^\t\n\f\r ]*?)>" // captures the XML tag
 	re := regexp.MustCompile(captureTagRegExp)            // necessary syntax for extracting captures
 
