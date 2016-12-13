ced69d8d4a8a56d37f5dd726db5fb0f94b5dab50
Daniel Alzugaray <dalzuga@gmail.com>
2016-12-13 10:19:38 -0800

    Fixed pagination issue where old values remained in the struct.

--- go_parser.go
+++ go_parser.go
@@ -10,39 +10,44 @@ import (
 	"strconv"
 )
 
-func main() {
-	fileBytes, err := ioutil.ReadFile("books.xml") // Read file into memory
+/*
+ * GRBQ stands for GoodReads Book Query
+ * GRAQ stands for GoodReads Author Query
+ */
 
+func main() {
+	fileBytes, err := ioutil.ReadFile("books.xml") // Read the GRBQ XML into memory
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	var grbq GoodReadsBookQuery
 
-	err = xml.Unmarshal(fileBytes, &grbq)
+	err = xml.Unmarshal(fileBytes, &grbq) // Parse the GBRQ XML to a GRBQ struct
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	AuthorID := grbq.Book.Authors[0].ID
+	AuthorID := grbq.Book.Authors[0].ID // Get the Author ID from the GRBQ struct
 
 	// fmt.Println("AuthorID:", AuthorID)
 
-	fileBytes, err = ioutil.ReadFile("authorlistbooks.xml")
+	/*
+	 * Here we are re-using fileBytes but for GRAQ, analogously
+	 */
+
+	fileBytes, err = ioutil.ReadFile("authorlistbooks.xml") // Read the GRAQ XML into memory
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	var graq GoodReadsAuthorQuery
 
-	err = xml.Unmarshal(fileBytes, &graq)
+	err = xml.Unmarshal(fileBytes, &graq) // Parse the GRAQ XML to a GRBQ struct
 
 	for _, bookValue := range graq.Author.Books.Book {
 		fmt.Println(bookValue.Title)
 	}
-
-	fmt.Println("start: ", graq.Author.Books.Start)
-	fmt.Println("end: ", graq.Author.Books.End)
-	fmt.Println("total: ", graq.Author.Books.Total)
-
-	fmt.Println("________________________________")
 
 	startBooks := graq.Author.Books.Start
 	endBooks := graq.Author.Books.End
@@ -53,12 +58,13 @@ func main() {
 	/* Code below is for pagination, need to code makeHTTPRequest */
 	pageNumber := 1
 	for totalBooks > endBooks {
+		var graqPagination GoodReadsAuthorQuery
 		fmt.Println("_______________________REQUEST________________________")
-		makeHTTPRequest("https://www.goodreads.com/author/list.xml", AuthorID, pageNumber, &graq)
-		startBooks = graq.Author.Books.Start
-		endBooks = graq.Author.Books.End
-		totalBooks = graq.Author.Books.Total
-		for _, bookValue := range graq.Author.Books.Book {
+		makeHTTPRequest("https://www.goodreads.com/author/list.xml", AuthorID, pageNumber, &graqPagination)
+		startBooks = graqPagination.Author.Books.Start
+		endBooks = graqPagination.Author.Books.End
+		totalBooks = graqPagination.Author.Books.Total
+		for _, bookValue := range graqPagination.Author.Books.Book {
 			fmt.Println(bookValue.Title)
 		}
 		pageNumber++
