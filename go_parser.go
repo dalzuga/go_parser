b70f500ea78c92b6d8ba38be0a226214579c4a28
Daniel Alzugaray <dalzuga@gmail.com>
2017-01-03 17:19:24 -0800

    Implemented a map[int]string. [save]

--- go_parser.go
+++ go_parser.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
+	"net/url"
 	"reflect"
+	"strconv"
 )
 
 /*
@@ -17,11 +20,13 @@ func main() {
 	var AuthorID int
 	getAuthorID("books.xml", &AuthorID)
 
-	bookTitles, err := requestAllBookTitles(AuthorID)
-	fmt.Println(reflect.TypeOf(bookTitles))
+	mapTitles, err := requestAllBookTitles(AuthorID)
+	fmt.Println(reflect.TypeOf(mapTitles))
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Println(mapTitles[0])
 }
 
 /*
@@ -45,8 +50,66 @@ func getAuthorID(fileName string, AuthorID *int) {
 	*AuthorID = grbq.Book.Authors[0].ID // Get the Author ID from the GRBQ struct
 }
 
-func requestAllBookTitles(AuthorID int) (map[string]int, error) {
+func requestAllBookTitles(AuthorID int) (map[int]string, error) {
 	fmt.Println(AuthorID)
+	endpointBase := "https://www.goodreads.com/author/list.xml"
+	page := 1
+	var startBooks, endBooks, totalBooks int
 
-	return make(map[string]int), nil
+	fmt.Println(startBooks, endBooks, totalBooks)
+
+	/*
+	 * Here, 'u' is a parsed object.
+	 */
+	u, err := url.Parse(endpointBase)
+	if err != nil {
+		return make(map[int]string), err
+	}
+
+	/*
+	 * Here, the query 'q' is formulated, for which RawString is the full URL
+	 */
+	q := u.Query()
+	q.Set("key", `kDkKnUxiz8cRBJhVjrtSA`)
+	q.Set("id", strconv.Itoa(AuthorID))
+	q.Set("page", strconv.Itoa(page))
+
+	/*
+	 * Here, 's' is our full constructed URL string
+	 */
+	u.RawQuery = q.Encode()
+	s := u.String()
+	fmt.Println(s)
+
+	req, err := http.NewRequest("GET", s, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	requestBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var graq GoodReadsAuthorQuery
+
+	err = xml.Unmarshal(requestBytes, &graq)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mapTitles := make(map[int]string)
+
+	for key, bookValue := range graq.Author.Books.Book {
+		mapTitles[key] = bookValue.Title
+	}
+
+	return mapTitles, nil
 }
