65257c6edb363f34eabbaadc031c32ab664d4367
Daniel Alzugaray <dalzuga@gmail.com>
2017-01-07 17:29:26 -0800

    Feature for searching API for author ID's with high pagination counts.

--- go_parser.go
+++ go_parser.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"time"
 )
 
 /*
@@ -41,8 +42,31 @@ func main() {
 
 	/* print the book titles */
 	mapLength := len(mapTitles)
-	for i := 0; i < mapLength; i++ {
-		fmt.Println(i+1, mapTitles[i])
+	// for i := 0; i < mapLength; i++ {
+	// 	fmt.Println(i+1, mapTitles[i])
+	// }
+	fmt.Println("Map titles obtained:", mapLength)
+
+	/* testing feature */
+	max := 1
+	for i := 116; i <= 1000; i++ {
+		time.Sleep(time.Second)
+		endpointBase := "https://www.goodreads.com/author/list.xml"
+		page := 1
+		_, more, err := requestPage(page, i, endpointBase)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if more > 0 {
+			fmt.Println("Current:", more)
+			fmt.Println("AuthorID:", i)
+		}
+		if more > max {
+			max = more
+			fmt.Println("_________________MAX_________________")
+			fmt.Println("Max:", max)
+			fmt.Println("AuthorID:", i)
+		}
 	}
 }
 
@@ -85,46 +109,46 @@ func requestAllBookTitles(AuthorID int) (map[int]string, error) {
 		fmt.Println("Additional requests needed:", more)
 	}
 
-	/* Make a map of channels, 1 channel per page */
-	channels := make(map[int]chan map[int]string)
-	for i := 2; i <= more+1; i++ {
-		channels[i] = make(chan map[int]string)
-	}
-
-	/* Make 'more' (number of) requests */
-	for i := 2; i <= more+1; i++ {
-		// channelMaps := make(chan map[int]string)
-		fmt.Println("For loop. i =", i)
-		go func(i int) {
-			fmt.Println("Go func. i =", i)
-			moreTitles, _, err := requestPage(i, AuthorID, endpointBase)
-			if err != nil {
-				fmt.Println("This request failed:", i)
-				channels[i] <- make(map[int]string)
-			} else {
-				fmt.Println("Received: page", i)
-				fmt.Println("111")
-				channels[i] <- moreTitles
-				fmt.Println("222")
-			}
-		}(i)
-	}
-
-	/* usually 30, but left variable in case API changes (untested) */
-	booksPerPage := len(mapTitles)
-
-	/* Receive pages in order */
-	for i := 2; i <= more+1; i++ {
-		moreTitles := <-channels[i]
-		fmt.Println("Receiving channel:", i)
-
-		/* add them to mapTitles (sequential) */
-		for j := 0; j <= booksPerPage; j++ {
-			if moreTitles[j] != "" {
-				mapTitles[(i-1)*booksPerPage+j] = moreTitles[j]
-			}
-		}
-	}
+	// /* Make a map of channels, 1 channel per page */
+	// channels := make(map[int]chan map[int]string)
+	// for i := 2; i <= more+1; i++ {
+	// 	channels[i] = make(chan map[int]string)
+	// }
+	//
+	// /* Make 'more' (number of) requests */
+	// for i := 2; i <= more+1; i++ {
+	// 	// channelMaps := make(chan map[int]string)
+	// 	fmt.Println("For loop. i =", i)
+	// 	go func(i int) {
+	// 		fmt.Println("Go func. i =", i)
+	// 		moreTitles, _, err := requestPage(i, AuthorID, endpointBase)
+	// 		if err != nil {
+	// 			fmt.Println("This request failed:", i)
+	// 			channels[i] <- make(map[int]string)
+	// 		} else {
+	// 			fmt.Println("Received: page", i)
+	// 			fmt.Println("111")
+	// 			channels[i] <- moreTitles
+	// 			fmt.Println("222")
+	// 		}
+	// 	}(i)
+	// }
+	//
+	// /* usually 30, but left variable in case API changes (untested) */
+	// booksPerPage := len(mapTitles)
+	//
+	// /* Receive pages in order */
+	// for i := 2; i <= more+1; i++ {
+	// 	moreTitles := <-channels[i]
+	// 	fmt.Println("Receiving channel:", i)
+	//
+	// 	/* add them to mapTitles (sequential) */
+	// 	for j := 0; j <= booksPerPage; j++ {
+	// 		if moreTitles[j] != "" {
+	// 			mapTitles[(i-1)*booksPerPage+j] = moreTitles[j]
+	// 		}
+	// 	}
+	// }
 
 	return mapTitles, nil
 }
